3ac79fc77ec1d78be774d392723b67ca15082385
linusfackler <78768883+linusfackler@users.noreply.github.com>
2022-04-14 15:45:18 -0500

    update

--- Coding 3/main.cpp
+++ Coding 3/main.cpp
@@ -6,20 +6,16 @@ using namespace std;
 
 int main()
 {
-    DFA test;
-    cout << "Enter DFA: " << endl;
-    cin >> test;
+    char choice = 'y';
+    while(choice == 'y')
+    {
+        cout << "enter string to check if DFA is valid: " << endl;
+        string k;
+        cin >> k;
+        DFA test3 = dfa_from_string(k);
 
-    cout << endl << "Your entered DFA: " << endl << test << endl;
-
-    string w = dfa_to_string(test);
-    cout << endl << endl << "now string to DFA: " << endl;
-    DFA test2 = dfa_from_string("00101010101010101");
-
-    cout << endl << test2 << endl;
-
-    cout << "DFA accepts itself: " << endl;
-    cout << dfa_accepts_self(test) << endl;
-
+        cout << "wanna try again?" << endl;
+        cin >> choice;
+    }
     return 0;
 }
