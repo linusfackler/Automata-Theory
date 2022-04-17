cd026f9ba1a49b819073f95115457db2e0da9124
linusfackler <78768883+linusfackler@users.noreply.github.com>
2022-04-17 14:29:07 -0500

    update

--- Coding 3/main.cpp
+++ Coding 3/main.cpp
@@ -6,33 +6,27 @@ using namespace std;
 
 int main()
 {
-    char choice = 'y';
-    while(choice == 'y')
-    {
+    DFA m;
+    
+    cout << "enter DFA: " << endl;
 
-        DFA m;
-	    
-        cout << "enter DFA: " << endl;
-        string k;
-        cin >> k;
-        stringstream (k) >> m;
+    cin >> m;
 
-        cout << m << endl;
+    cout << m << endl;
 
-        string p = dfa_to_string(m);
-        cout << endl << p << endl;
+    string p = dfa_to_string(m);
+    cout << endl << p << endl;
 
-        DFA yourmom = dfa_from_string(p);
+    DFA yourmom = dfa_from_string(p);
 
-        cout << endl << "DFA from string: " << endl;
-        cout << yourmom;
+    cout << endl << "DFA from string: " << endl;
+    cout << yourmom;
 
-        cout << endl << endl << "does it accept itself?" << endl;
-        cout << dfa_accepts_self(yourmom);
-        cout << endl << endl;
+    cout << endl << endl << "does it accept itself?" << endl;
+    cout << dfa_accepts_self(yourmom);
+    cout << endl << endl;
 
-        cout << "wanna try again?" << endl;
-        cin >> choice;
-    }
+    // cout << "wanna try again?" << endl;
+    // cin >> choice;
     return 0;
 }
